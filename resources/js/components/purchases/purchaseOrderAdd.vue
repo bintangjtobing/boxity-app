88df38a4bc510b8374752f230403955d4e078356
Bintang Tobing <hello@bintangtobing.com>
2021-06-16 10:09:02 +0700

    purchase order add vue component

--- resources/js/components/purchases/purchaseOrderAdd.vue
+++ resources/js/components/purchases/purchaseOrderAdd.vue
@@ -10,7 +10,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>  
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
