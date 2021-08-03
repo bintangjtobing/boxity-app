fb8ffadd5b3c8cf31db2b2f6c4ea603289377219
Bintang Tobing <hello@bintangtobing.com>
2021-08-04 00:37:35 +0700

    fix: change the design of report

--- resources/views/vendor/invoices/templates/sales-order.blade.php
+++ resources/views/vendor/invoices/templates/sales-order.blade.php
@@ -1,287 +1,288 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>Report Sales Order</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <header class="clearfix">
-      <div id="logo">
-        <img src="{{ $image }}">
-      </div>
-      <div id="company">
-        <h2 class="name">{{ $companyName }}</h2>
-        <div>{{ $companyAddress }}</div>
-        <div>{{ $companyPhone }}</div>
-        <div><a href=<?php echo "mailto:".$companyEmail ?>>{{ $companyEmail }}</a></div>
-      </div>
-      </div>
+        <div id="logo">
+            <img src="{{ $image }}">
+        </div>
+        <div id="company">
+            <h2>{{ $companyName }}</h2>
+            <div>{{ $companyAddress }}</div>
+            <div>{{ $companyPhone }}</div>
+            <div><a href=<?php echo "mailto:".$companyEmail ?>>{{ $companyEmail }}</a></div>
+        </div>
     </header>
     <main>
-      <div id="details" class="clearfix">
-        <div id="client">
-          <div class="to">INVOICE TO:</div>
-          <h2 class="name">{{ $customerName }}</h2>
-          <div class="address">{{ $customerAddress }}</div>
-          <div class="email"><a href=<?php echo "mailto:".$customerEmail ?>>{{ $customerEmail }}</a></div>
+        <div id="details" class="clearfix">
+            <div id="client">
+                <div class="to">INVOICE TO:</div>
+                <h2>{{ $customerName }}</h2>
+                <div class="address">{{ $customerAddress }}</div>
+                <div class="email"><a href=<?php echo "mailto:".$customerEmail ?>>{{ $customerEmail }}</a></div>
+            </div>
+            <div id="invoice">
+                <h1>{{ $soNumber }}</h1>
+                <div class="date">Order Date: {{ $orderDate }}</div>
+                <!-- <div class="date">Due Date: 30/06/2014</div> -->
+            </div>
         </div>
-        <div id="invoice">
-          <h1>{{ $soNumber }}</h1>
-          <div class="date">Date of Invoice: {{ $orderDate }}</div>
-          <!-- <div class="date">Due Date: 30/06/2014</div> -->
-        </div>
-      </div>
-      <table border="0" cellspacing="0" cellpadding="0">
-        <thead>
-          <tr>
-            <th class="no">#</th>
-            <th class="desc">DESCRIPTION</th>
-            <th class="unit">UNIT PRICE</th>
-            <th class="qty">QUANTITY</th>
-            <th class="total">TOTAL</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php 
+        <table border="0" cellspacing="0" cellpadding="0">
+            <thead>
+                <tr>
+                    <th class="desc">Description</th>
+                    <th class="price">Price</th>
+                    <th class="qty">Quantity</th>
+                    <th class="price">Unit</th>
+                    <th class="total">Sub Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
             $i = 0; $total = 0;
             foreach ($items as $item) {
           ?>
-          <tr>
-            <td class="no">{{ $i }}</td>
-            <td class="desc"><h3>{{ $item['name'] }}</h3>{{ $item['remark'] }}</td>
-            <td class="unit">Rp. {{ $item['price'] }}</td>
-            <td class="qty">{{ $item['qty'] }}</td>
-            <td class="total">Rp. {{ $item['priceAmount'] }}</td>
-          </tr>
-          <?php 
-              $total = +$item['priceAmount'];
+                <tr>
+                    <td class="desc">
+                        {{ $item['name'] }}{{ $item['remark'] }}
+                    </td>
+                    <td class="price">Rp. {{ $item['price'] }}</td>
+                    <td class="qty">{{ $item['qty'] }}</td>
+                    <td class="unit">{{ $item['unit'] }}</td>
+                    <td class="total">Rp. {{ $item['priceAmount'] }}</td>
+                </tr>
+                <?php
+              $total = $total+$item['priceAmount'];
               $i++;
-            } 
+            }
           ?>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="2"></td>
-            <td colspan="2">GRAND TOTAL</td>
-            <td>Rp. {{ $total }}</td>
-          </tr>
-        </tfoot>
-      </table>
-      <div id="thanks">Thank you!</div>
-      <div id="notices">
-        <div>NOTICE:</div>
-        <div class="notice">{{ $remark }}</div>
-      </div>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="4">Total</td>
+                    <td>Rp. {{ $total }}</td>
+                </tr>
+            </tfoot>
+        </table>
+        <div id="notices">
+            <div>Notes:</div>
+            <div class="notice">{{ $remark }}</div>
+        </div>
     </main>
     <footer>
-      Invoice was created on a computer and is valid without the signature and seal.
+        Invoice was created on a computer and is valid without the signature and seal.
     </footer>
-  </body>
+</body>
+
 </html>
 
 <style>
-  @font-face {
-  font-family: SourceSansPro;
-  src: url(SourceSansPro-Regular.ttf);
-}
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap');
 
-.clearfix:after {
-  content: "";
-  display: table;
-  clear: both;
-}
+    @font-face {
+        font-family: 'Lato', sans-serif;
+    }
 
-a {
-  color: #0087C3;
-  text-decoration: none;
-}
+    .clearfix:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
 
-body {
-  position: relative;
-  height: 21cm; 
-  margin: 0 auto;
-  color: #555555;
-  background: #FFFFFF; 
-  font-family: Arial, sans-serif; 
-  font-size: 14px; 
-  font-family: SourceSansPro;
-}
+    a {
+        color: #f35e00;
+        text-decoration: none;
+    }
 
-header {
-  padding: 10px 0;
-  margin-bottom: 20px;
-  border-bottom: 1px solid #AAAAAA;
-}
+    body {
+        position: relative;
+        height: 21cm;
+        margin: 0 auto;
+        color: #777;
+        background: #FFFFFF;
+        font-family: 'Lato', sans-serif;
+    }
 
-#logo {
-  float: left;
-  margin-top: 8px;
-}
+    header {
+        padding: 10px 0;
+        margin-bottom: 20px;
+        border-bottom: 1px solid #AAAAAA;
+    }
 
-#logo img {
-  height: 70px;
-}
+    #logo {
+        float: left;
+        margin-top: 8px;
+    }
 
-#company {
-  width: 45%;
-  float: right;
-  text-align: right;
-}
+    #logo img {
+        height: 100px;
+    }
 
+    #company {
+        width: 45%;
+        float: right;
+        font-size: .85rem;
+        text-align: right;
+    }
 
-#details {
-  margin-bottom: 50px;
-}
+    #details {
+        margin-bottom: 50px;
+    }
 
-#client {
-  width: 40%;
-  padding-left: 6px;
-  border-left: 6px solid #0087C3;
-  float: left;
-}
+    #client {
+        width: 40%;
+        padding-left: 6px;
+        border-left: 6px solid #f35e00;
+        float: left;
+    }
 
-#client .to {
-  color: #777777;
-}
+    #client .to {
+        font-size: .85rem;
+        color: #777777;
+    }
 
-#client .address {
-  font-size: 11px;
-}
+    #client .address,
+    #client .email {
+        font-size: .85rem;
+    }
 
-h2.name {
-  font-size: 1.4em;
-  font-weight: normal;
-  margin: 0;
-}
+    #company h2,
+    #client h2 {
+        font-size: 1.2rem;
+        font-weight: bolder;
+        margin: 0;
+    }
 
-#invoice {
-  width: 100%;
-  float: right;
-  text-align: right;
-}
+    #invoice {
+        width: 100%;
+        float: right;
+        text-align: right;
+    }
 
-#invoice h1 {
-  color: #0087C3;
-  font-size: 2.4em;
-  line-height: 1em;
-  font-weight: normal;
-  margin: 0  0 10px 0;
-}
+    #invoice h1 {
+        color: #f35e00;
+        font-size: 1.2rem;
+        line-height: 1em;
+        font-weight: normal;
+        margin: 0 0 2px 0;
+    }
 
-#invoice .date {
-  font-size: 1.1em;
-  color: #777777;
-}
+    #invoice .date {
+        font-size: 1rem;
+        color: #777777;
+    }
 
-table {
-  width: 100%;
-  border-collapse: collapse;
-  border-spacing: 0;
-  margin-bottom: 20px;
-}
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        border-spacing: 0;
+        margin-bottom: 20px;
+    }
 
-table th,
-table td {
-  padding: 20px;
-  background: #EEEEEE;
-  text-align: center;
-  border-bottom: 1px solid #FFFFFF;
-}
+    table th,
+    table td {
+        padding: 20px;
+        text-align: center;
+        border-bottom: 1px solid #FFFFFF;
+    }
 
-table th {
-  white-space: nowrap;        
-  font-weight: normal;
-}
+    table th {
+        white-space: nowrap;
+        font-weight: normal;
+    }
 
-table td {
-  text-align: right;
-}
+    table td {
+        text-align: right;
+    }
 
-table td h3{
-  color: #57B223;
-  font-size: 1.2em;
-  font-weight: normal;
-  margin: 0 0 0.2em 0;
-}
+    table .desc {
+        text-align: left;
+    }
 
-table .no {
-  color: #FFFFFF;
-  font-size: 1.6em;
-  background: #57B223;
-}
+    table .qty {
+        text-align: center;
+    }
 
-table .desc {
-  text-align: left;
-}
+    thead tr {
+        background: #f35e00;
+        color: #fff;
+    }
 
-table .unit {
-  background: #DDDDDD;
-}
+    tbody {
+        background: rgb(246, 246, 246);
+    }
 
-table .qty {
-}
+    thead tr th {
+        font-weight: 700;
+        font-size: .90rem;
+    }
 
-table .total {
-  background: #57B223;
-  color: #FFFFFF;
-}
+    thead tr .price,
+    thead tr .total {
+        text-align: right;
+    }
 
-table td.unit,
-table td.qty,
-table td.total {
-  font-size: 1.2em;
-}
-/* 
+    tbody tr .desc {
+        color: #777;
+    }
+
+    tbody tr {
+        color: #777;
+    }
+
+    /*
 table tbody tr:last-child td {
   border: none;
 } */
 
-table tfoot td {
-  padding: 10px 20px;
-  background: #FFFFFF;
-  border-bottom: none;
-  font-size: 1.2em;
-  white-space: nowrap; 
-  border-top: 1px solid #AAAAAA; 
-}
+    table tfoot td {
+        padding: 10px 20px;
+        background: #FFFFFF;
+        border-bottom: none;
+        font-size: .90rem;
+        white-space: nowrap;
+        border-top: 1px solid #AAAAAA;
+    }
 
-table tfoot tr:first-child td {
-  border-top: none; 
-}
+    table tfoot tr:first-child td {
+        border-top: none;
+    }
 
-table tfoot tr:last-child td {
-  color: #fff;
-  font-size: 1.4em;
-  border-top: 1px solid #fff; 
-  background: #57B223;
-}
+    table tfoot tr:last-child td {
+        color: #fff;
+        font-size: .90rem;
+        font-weight: 700;
+        border-top: 1px solid #fff;
+        background: #f35e00;
+    }
 
-table tfoot tr td:first-child {
-  border: none;
-}
+    table tfoot tr td:first-child {
+        border: none;
+    }
 
-#thanks{
-  font-size: 2em;
-  margin-bottom: 50px;
-}
+    #notices {
+        padding-left: 6px;
+        border-left: 6px solid #f35e00;
+    }
 
-#notices{
-  padding-left: 6px;
-  border-left: 6px solid #0087C3;  
-}
+    #notices .notice {
+        font-size: .90rem;
+    }
 
-#notices .notice {
-  font-size: 1.2em;
-}
+    footer {
+        color: #777777;
+        width: 100%;
+        height: 30px;
+        position: absolute;
+        bottom: 0;
+        border-top: 1px solid #AAAAAA;
+        padding: 8px 0;
+        text-align: center;
+    }
 
-footer {
-  color: #777777;
-  width: 100%;
-  height: 30px;
-  position: absolute;
-  bottom: 0;
-  border-top: 1px solid #AAAAAA;
-  padding: 8px 0;
-  text-align: center;
-}
 </style>
