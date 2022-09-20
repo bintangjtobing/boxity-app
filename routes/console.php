e0ddbfe32c3310d2ae42adddc69fac98cf6a2749
bintangjtobing <hello@bintangtobing.com>
2022-09-20 21:10:22 +0700

    update: logs clear

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+Artisan::command('logs:clear', function () {
+    exec('rm -f ' . storage_path('logs/*.log'));
+    exec('rm -f ' . base_path('*.log'));
+    $this->comment('Logs have been cleared!');
+})->describe('Clear log files');
